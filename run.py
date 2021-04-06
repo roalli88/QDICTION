034681a9616b77bb2e9818d8c9e3fb1e54cbf5af
roalli <roalli@admin88.net>
2021-04-06 16:32:06 +0100

    removed unnecessary lines from run.py

--- run.py
+++ run.py
@@ -1,11 +1,3 @@
 from qdiction.__main__ import main
-import json
-import nltk
-
-def config_checker():
-    
-    nltk.download('wordnet')
-    with open("config.json", "r+") as f:
-        
 
 main()
